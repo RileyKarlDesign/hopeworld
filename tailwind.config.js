f080719d8da12490555ccbbc3ad4527286eac812
Riley Karl <rileykarldesign@gmail.com>
2024-05-01 22:51:11 +0100

    hello

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content:{
+    relative: true, 
+    files: [
+    "./assets/**/*.{js,ts,jsx,tsx,mdx,liquid}",
+    "./theme/**/*.{js,ts,jsx,tsx,mdx,liquid}",
+    "./sections/**/*.{js,ts,jsx,tsx,mdx,liquid}",
+    "./snippets/**/*.{js,ts,jsx,tsx,mdx,liquid}",
+    
+   
+ 
+  ],},
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
